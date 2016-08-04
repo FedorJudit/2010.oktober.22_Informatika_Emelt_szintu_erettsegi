a3401ec07954f3a6b2cc9e1f2c7c96a190422e7f
Zoltan Fedor <zoltan.0.fedor@gmail.com>
2016-08-03 21:20:33 -0400

    Apro javitasok es a 7. feladat ujracsinalasa

--- anagram.py
+++ anagram.py
@@ -13,15 +13,15 @@ print("A beirt szoveg {} kulonbozo karaktert tartalmaz".format(len(szoveg_bekere
 print("2. feladat")
 """Szavak beolvasasa a szotar.txt--bol es eltarolasa amit en szotarban oldanak meg. Ami a kovetkezo keppen nezne ki:
 szotar{
-hanyadik szo{
-"Szo"=eszesen
-"szo karakterekent" = [e,s,z,e,s,e,n]
+hanyadik szo: {
+    "Szo": eszesen,
+    "szo karakterekent": [e,s,z,e,s,e,n],
+    "anagramma": [e,n,s,z]  # egyedi betuk abc sorrendben
  }
 }
 """
 szotar = {}
 n = 0
-sortor = []
 with open("szotar.txt", "rt+", encoding="ASCII") as f:
     for s in f:
         n+=1
@@ -29,17 +29,17 @@ with open("szotar.txt", "rt+", encoding="ASCII") as f:
         szotar[n] = {}
         szotar[n]["szo"] = sor
         szotar[n]["anagramma"] = anagramma(sor)
+        sortor = []
         for sz in sor:
             sortor.append(sz)
             szotar[n]["szo karakterenkent"] = sortor
-        sortor = []
 
 #print(szotar)
 print("3. feladat")
 """Minden szo karaktereit egyenkent abc sorrendbe kell tenni es ezt ki kell irni a abc.txt alomanyba.(minden sorhoz kell tartoznia egy erteknek) """
 with open("abc.txt", "wt", encoding="ASCII") as f:
     for szo in szotar.values():
-        f.write(str(sorted(szo["szo karakterenkent"]))+"\n")
+        f.write(''.join(sorted(szo["szo karakterenkent"]))+"\n")
 
 print("4. feladat")
 """Be kell kerni a felhasznalotol 2 szot es meg kell hataronzi hogy anagramma-e egymasnak a ket szo."""
@@ -56,7 +56,6 @@ else:
 print("5. feladat")
 """Felhasznalotol egy szo bekeres es az allomany szavaibol megkeresni hogy ha van talalat akkor egymas ala kiirni."""   
 szokereso = list(input("Kerem irjon be egy szot aminek az anagrammajat megkeresi a szotar alomanyban: "))
-anagrammak = []
 n = 0
 for a in szotar.values():
     if anagramma(szokereso) == a["anagramma"]:
@@ -71,27 +70,45 @@ szotarleghosszabb = {}
 for k,v in szotar.items():
     szotarleghosszabb[k] = len(v["szo"])
 for d,f in szotarleghosszabb.items():
-    if f == int(max(sorted(szotarleghosszabb.values()))):
-        for k in szotar.keys():
-            if k == d:
-                print(szotar[k]["szo"])
+    if f == max(szotarleghosszabb.values()):
+        print(szotar[d]["szo"])
 
 print("7. feladat")
 """karakter szam szerinti novekvo sorrendbe rendezes, egymas amagramma szavai keruljenek egy sorba szokozzel elvalasztva nem ugyanaz a karaktert \
 tartalmazo lista szamvai keruljenek egy sorba kulonboyo hasszusagu szavak egy sor sortoressel keruljenek az alomanyba  """
-elozo_hossza = int()
-elozo_szo = str()
-liest = []
-df = []
+
+
+""" Itt csinalok egy uj szotarat, ahol a kulcs a szo hossza es a belso konyvtar kulcsa az
+anagramma osszefuzve egy sztringe:
+szavak = {
+    4: {
+        'akku': ['kuka','akku'],
+        'aipp': ['papi', 'pipa']
+    },
+    5: {
+        ...
+    }
+}"""
+
+# toltsuk fel ezt az uj szotarat a 'szotar' nevu szotarbol
+szavak = {}
+for k,v in szotar.items():
+    hossz = len(v['szo'])
+    if hossz not in szavak:
+        szavak[hossz] = {}
+    anagramma = ''.join(v['anagramma'])
+    if anagramma not in szavak[hossz]:
+        szavak[hossz][anagramma] = []
+    szavak[hossz][anagramma].append(v['szo'])
+
+#print(szavak)
+
+""" most pedig csak kiirom ezt a szotarat """
 with open("rendezve.txt","wt", encoding="ASCII") as f:
-    for k in szotar.values():
-        for a in range(2, int(max(sorted(szotarleghosszabb.values())))):
-            if len(k["szo"]) == a:      
-                if str(k["anagramma"]) == elozo_szo:
-                    df.append(k['szo'])
-                else:
-                    print(k['szo'])
-                    elozo_szo = str(k["anagramma"])
-                print(df)
-            else:
-                df = []
+    elozo_hossz = 0
+    for k,v in sorted(szavak.items()):
+        if elozo_hossz != 0 and elozo_hossz != k:
+            f.write("\n")
+        for k2, v2 in v.items():
+            f.write(' '.join(v2)+'\n')
+        elozo_hossz = k
